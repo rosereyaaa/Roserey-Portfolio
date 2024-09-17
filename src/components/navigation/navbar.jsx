18284c0db583f75a9c0110fb53edeb249e63b2a3
Rosérey <rhyearaven@gmail.com>
2024-09-17 22:14:15 +0800

    update of file location

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -8,12 +8,13 @@ const Navbar = () => {
   return (
     <header className="navbarSection">
       <nav className="navbarRectangle">
-        <img 
+        <a className='rc-logo'>
+          <img 
           loading="lazy" 
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/78a440521ecab22b8e4b7621289cb77af9e39b6ffeee2c1aaebd39dc541e906a?placeholderIfAbsent=true&apiKey=34e9e65e881d49b1ab7778333f519a8d" 
+          src="./rc-logo.svg" 
           className="logo" 
-          alt="Company logo" 
         />
+        </a>
         <div className="navLinks">
           {navItems.map((item, index) => (
             <NavItem key={index} text={item} />
