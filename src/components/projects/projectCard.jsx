6de10d8c87f3e507ac53985e5e041b8f66a3fef2
Rosérey <rhyearaven@gmail.com>
2024-09-18 23:45:42 +0800

    linking of github projects

--- src/components/projects/projectCard.jsx
+++ src/components/projects/projectCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './style.module.css';
 
-const projectCard = ({ name, image, description, technologies }) => {
+const projectCard = ({ name, image, link, description, technologies }) => {
     return (
     <article className={styles.projectCard}>
         <div className={styles.projectHeader}>
             <h3 className={styles.projectName}>{name}</h3>
-            <img src={image} alt={`${name} project icon`} className={styles.projectIcon} />
+            <a href={`https://github.com/${link}`} target="_blank" rel="noopener noreferrer">
+                <img src={image} alt={`${name} project icon`} className={styles.projectIcon} />
+            </a>
         </div>
         <p className={styles.projectDescription}>{description}</p>
         <ul className={styles.technologiesList}>
--- src/components/projects/projectSection.jsx
+++ src/components/projects/projectSection.jsx
@@ -6,18 +6,21 @@ const projectsData = [
     { 
         name: "Treats Delight",
         image: "./github-button.svg",
+        link:"DoMAAA21/TreatsDelightTS",
         description: "This project partially fulfills the requirement for our undergraduate degree under the IT Capstone course at the Technological University of the Philippines-Taguig. The comprehensive system goes beyond simple canteen management. It encompasses efficient ordering and administrative control systems to ensure smooth operations between the canteen staff, students, faculty, and admin staff of TUP-Taguig.",
         technologies: ["React", "TypeScript", "TailwindCSS", "NodeJS", "ExpressJS", "Vite", "MongoDB"]
     },
     {
         name: "BhieCycle",
         image: "./github-button.svg",
+        link:"rosereyaaa/BhieCycle",
         description: "A web development project that is a requirement for the Programming Language-Design and Implementation course at the Technological University of the Philippines-Taguig. It is an e-commerce platform designed for bike enthusiasts and regular cyclists. Users have the option to create accounts and purchase bike accessories and services.",
         technologies: ["React", "JavaScript", "NodeJS", "MongoDB"]
     },
     {
         name: "WonderPets",
         image: "./github-button.svg",
+        link:"rosereyaaa/WonderPets",
         description: "WonderPets is an e-commerce management system project during my sophomore year for the Advance Web Application course at the Technological University of the Philippines - Taguig. The system provides a range of services tailored to the needs of pet owners, including grooming and regular check-ups to ensure the well-being of their beloved pets.",
         technologies: ["Laravel", "PHP"]
     }
