db50fd335114df235a1e9e69bc66fde1abe6e5c4
Rosérey <rhyearaven@gmail.com>
2024-09-18 15:29:39 +0800

    adding of about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import Navbars from './components/navigation/navbar'
 import React from 'react';
+import './App.css'
+
+// Initialization of pages 
+import Navbars from './components/navigation/navbar'
+import HeaderSection from './components/aboutMe/header';
 
 function App() {
   return (
     <>
-    <div className="flex flex-col text-xl font-medium text-indigo-500">
+    <div className="main">
       <Navbars/>
+      <HeaderSection/>
     </div>
     </>
   )
