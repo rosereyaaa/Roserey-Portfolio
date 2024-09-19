bdd26b361eb09eebfd83bf339a1c66258a4cb028
Rosérey <rhyearaven@gmail.com>
2024-09-19 14:57:43 +0800

    navigation css update

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './style.module.css';
 import NavItem from './navitem';
 
-const navbar = () => {
+const Navbar = () => {
   const navItems = [
     { text: 'About Me', link: '#about-me' },
     { text: 'Projects', link: '#projects' },
@@ -10,24 +10,44 @@ const navbar = () => {
     { text: 'Resume', link: 'https://drive.google.com/file/d/1exNFp2ahis-XjLY3Jm0hrVrdB1HUZoz_/view?usp=sharing' },
   ];
 
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <header className={styles.navbarSection}>
       <nav className={styles.navbarRectangle}>
+
         <a className="rc-logo">
-          <img 
-          loading="lazy" 
-          src="./rc-logo.svg" 
-          className={styles.logo} 
-        />
+          <img loading="lazy" src="./rc-logo.svg" className={styles.logo} />
         </a>
-        <div className={styles.navLinks}>
+
+        <div className={styles.navLinks} style={{ display: menuOpen || !isSmallScreen ? 'flex' : 'none' }}>
           {navItems.map((item, index) => (
             <NavItem key={index} text={item.text} link={item.link} />
           ))}
         </div>
+
+        <img
+          className={styles.menuBtn}
+          src={menuOpen ? "./close-icon.svg" : "./menu-icon.svg"}
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+          style={{ display: isSmallScreen ? 'block' : 'none' }}
+        />
+        
       </nav>
     </header>
   );
 };
 
-export default navbar;
+export default Navbar;
