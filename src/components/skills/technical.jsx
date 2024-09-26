c6f83f77e6f5c5da1129ebec0d231a231cc35463
Rosérey <rhyearaven@gmail.com>
2024-09-26 10:24:23 +0800

    add of other skills

--- src/components/skills/technical.jsx
+++ src/components/skills/technical.jsx
@@ -17,6 +17,31 @@ const TechnicalSkills = () => {
         { iconSrc: "./laravel-logo.svg", name: "Laravel" }
     ];
     
+    const databases = [
+        {iconSrc: "./mysql-logo.svg", name: "MySQL"},
+        {iconSrc: "./mongodb-logo.svg", name: "MongoDB"}
+    ];
+
+    const pmt = [
+        {iconSrc: "./jira-logo.svg", name: "Jira"},
+        {iconSrc: "./github-logo.svg", name: "GitHub"}
+    ];
+
+    const designTools = [
+        {iconSrc: "./figma-logo.svg", name: "Figma"},
+        {iconSrc: "./canva-logo.svg", name: "Canva"}
+    ];
+
+    const productivitySuites = [
+        {iconSrc: "./g-docs-logo.svg", name: "Google Docs"},
+        {iconSrc: "./g-slides-logo.svg", name: "Google Slides"},
+        {iconSrc: "./g-sheets-logo.svg", name: "Google Sheets"},
+        {iconSrc: "./ms-word-logo.svg", name: "Microsoft Word"},
+        {iconSrc: "./ms-ppt-logo.svg", name: "Microsoft PowerPoint"},
+        {iconSrc: "./ms-excel-logo.svg", name: "Microsoft Excel"},
+    ];
+
+
     return (
     <section className={styles.skillsSection}>
         <div className={styles.mainContent}>
@@ -26,9 +51,14 @@ const TechnicalSkills = () => {
                     <div className={styles.pinkBox}>
                         <SkillSection title="Programming Languages" skills={programmingLanguages} />
                         <SkillSection title="Frameworks" skills={frameworks} />
+                        <SkillSection title="Databases" skills={databases} />
+                        <SkillSection title="Project Management Tools" skills={pmt} />
+                        <SkillSection title="Design Tools" skills={designTools} />
+                        <SkillSection title="Productivity Suites" skills={productivitySuites} />
                     </div>
                 </div>
             </div>
+            <div className={styles.pinkLine} />
         </div>
     </section>
     );
