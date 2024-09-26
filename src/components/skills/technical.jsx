3248be6208faac7dc0ec9141e2f222ae01f164a3
Rosérey <rhyearaven@gmail.com>
2024-09-26 19:51:47 +0800

    refactor of css styles

--- src/components/skills/technical.jsx
+++ src/components/skills/technical.jsx
@@ -1,49 +1,39 @@
 import React from 'react';
-import SkillSection from './skillSection';
 import styles from './style.module.css';
+import SkillSection from './skillSection';
 
-const TechnicalSkills = () => {
-    const programmingLanguages = [
-        { iconSrc: "./html-logo.svg", name: "HTML" },
-        { iconSrc: "./css-logo.svg", name: "CSS" },
-        { iconSrc: "./javascript-logo.svg", name: "JavaScript" },
-        { iconSrc: "./php-logo.svg", name: "PHP" },
-        { iconSrc: "./java-logo.svg", name: "Java" }
-    ];
+const programmingLanguages = [
+    { iconSrc: "./html-logo.svg", name: "HTML" },
+    { iconSrc: "./css-logo.svg", name: "CSS" },
+    { iconSrc: "./javascript-logo.svg", name: "JavaScript" },
+    { iconSrc: "./php-logo.svg", name: "PHP" },
+    { iconSrc: "./java-logo.svg", name: "Java" }
+];
 
-    const frameworks = [
-        { iconSrc: "./react-logo.svg", name: "React" },
-        { iconSrc: "./nodejs-logo.svg", name: "NodeJS" },
-        { iconSrc: "./laravel-logo.svg", name: "Laravel" }
-    ];
-    
-    const databases = [
-        {iconSrc: "./mysql-logo.svg", name: "MySQL"},
-        {iconSrc: "./mongodb-logo.svg", name: "MongoDB"}
-    ];
+const frameworks = [
+    { iconSrc: "./react-logo.svg", name: "React" },
+    { iconSrc: "./nodejs-logo.svg", name: "NodeJS" },
+    { iconSrc: "./laravel-logo.svg", name: "Laravel" }
+];
 
-    const pmt = [
-        {iconSrc: "./jira-logo.svg", name: "Jira"},
-        {iconSrc: "./github-logo.svg", name: "GitHub"}
-    ];
+const databases = [
+    {iconSrc: "./mysql-logo.svg", name: "MySQL"},
+    {iconSrc: "./mongodb-logo.svg", name: "MongoDB"}
+];
 
-    const designTools = [
-        {iconSrc: "./figma-logo.svg", name: "Figma"},
-        {iconSrc: "./canva-logo.svg", name: "Canva"}
-    ];
+const pmt = [
+    {iconSrc: "./jira-logo.svg", name: "Jira"},
+    {iconSrc: "./github-logo.svg", name: "GitHub"}
+];
 
-    const productivitySuites = [
-        {iconSrc: "./g-docs-logo.svg", name: "Google Docs"},
-        {iconSrc: "./g-slides-logo.svg", name: "Google Slides"},
-        {iconSrc: "./g-sheets-logo.svg", name: "Google Sheets"},
-        {iconSrc: "./ms-word-logo.svg", name: "Microsoft Word"},
-        {iconSrc: "./ms-ppt-logo.svg", name: "Microsoft PowerPoint"},
-        {iconSrc: "./ms-excel-logo.svg", name: "Microsoft Excel"},
-    ];
+const designTools = [
+    {iconSrc: "./figma-logo.svg", name: "Figma"},
+    {iconSrc: "./canva-logo.svg", name: "Canva"}
+];
 
-
+function TechnicalSkills() {
     return (
-    <section className={styles.skillsSection}>
+        <section className={styles.skillsSection}>
         <div className={styles.mainContent}>
             <div className={styles.contentWrapper}>
                 <h2 className={styles.sectionTitle}>Technical Skills</h2>
@@ -54,7 +44,6 @@ const TechnicalSkills = () => {
                         <SkillSection title="Databases" skills={databases} />
                         <SkillSection title="Project Management Tools" skills={pmt} />
                         <SkillSection title="Design Tools" skills={designTools} />
-                        <SkillSection title="Productivity Suites" skills={productivitySuites} />
                     </div>
                 </div>
             </div>
@@ -62,6 +51,6 @@ const TechnicalSkills = () => {
         </div>
     </section>
     );
-};
+}
 
 export default TechnicalSkills;
